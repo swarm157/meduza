48ad6e97b6dbda1a808b9d7c7e0a80f380a1994f
Yra Kurbatov <108115642+swarm157@users.noreply.github.com>
2025-02-05 22:09:45 +0400

    Update script.js

--- yra/script.js
+++ yra/script.js
@@ -4,8 +4,8 @@ window.onload = main;
 
 let matrix = [];
 
-let width = 25;
-let height = 25;
+let width = 128;
+let height = 128;
 
 class Bot {
     constructor(x, y, energy, code) {
@@ -20,21 +20,40 @@ class Bot {
     next() {
         if(this.state>this.code.length)
             this.state = 0;
-        switch(this.state) {
-            case 0: break;
-            case 1: this.energy+=8; break;
-            case 2: this.energy=0; break;
-            case 3: {
-                if()
-            } break;
+        try {
+            switch(this.state) {
+                case 0: break;
+                case 1: this.energy+=8; break;
+                //case 2: this.energy=0; break;
+                case 3: this.swap(this.left()); break;
+                case 4: this.swap(this.right()); break;
+                case 5: this.swap(this.up()); break;
+                case 6: this.swap(this.down()); break;
+                case 7: this.eat(this.left()); break;
+                case 8: this.eat(this.right()); break;
+                case 9: this.eat(this.up()); break;
+                case 10: this.eat(this.down()); break;
+                case 11: this.born(this.down()); break;
+                case 12: this.born(this.left()); break;
+                case 13: this.born(this.up()); break;
+                case 14: this.born(this.right()); break;
+                case 15: break;
+            }
+        } catch (error) {
+            
         }
+        
         if(this.energy<=0||this.health<=0) {
             this.energy = 0;
             this.alive = false;
         } else if(this.energy>100) {
-            let d = 100-this.energy;
-            this.energy  = 100;
-            this.health -= d;
+
+            this.energy-=3;
+            this.health+=3;
+        }
+        if(this.health>100) {
+            this.code[Math.round(Math.random()*this.code.length)]+=Math.round(Math.random()*10)-5;
+            this.health-=3;
         }
         if(this.alive)
             this.energy--;
@@ -42,28 +61,32 @@ class Bot {
     }
 
     left() {
-        if(x<0) {
-            return matrix[width-1][y];
+        if(this.x<0) {
+            return matrix[width-1][this.y];
         }
-        return matrix[x-1][y];
+        try {
+            return matrix[this.x-1][this.y];
+        } catch (error) {
+            return matrix[this.x-1][this.y+1];
+        }
     }
     right() {
-        if(x>width) {
-            return matrix[0][y];
+        if(this.x>width) {
+            return matrix[0][this.y];
         }
-        return matrix[x+1][y];
+        return matrix[this.x+1][this.y];
     }
     up() {
-        if(y<0) {
-            return matrix[x][height-1];
+        if(this.y<0) {
+            return matrix[this.x][height-1];
         }
-        return matrix[x][y-1];
+        return matrix[this.x][this.y-1];
     }
     down() {
-        if(x>width) {
-            return matrix[0][y];
+        if(this.y>height) {
+            return matrix[this.x][0];
         }
-        return matrix[x][y+1];
+        return matrix[this.x][this.y+1];
     }
     swap(bot) {
         if(this.energy>bot.energy) {
@@ -73,14 +96,57 @@ class Bot {
             bot.copyTo(t);
             this.copyTo(bot);
             t.copyTo(this);
+            t.x = bot.x;
+            t.y = bot.y;
+            bot.x = this.x;
+            bot.y = this.y;
+            this.x = t.x;
+            this.y = t.y;
+        } else {
+            this.health/=1.75;
+            this.energy-=bot.energy/10;
+            bot.health/=1.25;
         }
     }
     eat(bot) {
-
+        if(this.energy+this.health>bot.energy+bot.health) {
+            this.energy+=bot.energy/1.25;
+            this.health+=bot.health/2;
+            bot.health=0;
+            bot.energy=0;
+        } else {
+            this.energy+=(bot.energy+bot.health)-(this.energy+this.health);
+            this.health+=(bot.energy+bot.health)-(this.energy+this.health);
+        }
     }
 
     copyTo(bot) {
+        bot.x = this.x;
+        bot.y = this.y;
+        bot.health = this.health;
+        //bot.code = this.code;
+        for(let i = 0; i < this.code.length; i++) {
+            bot.code[i] = this.code[i];
+        }
+        bot.energy = this.energy;
+        bot.state = this.state;
+        bot.alive = this.alive;
+    }
 
+    born(bot) {
+        if(!bot.alive) {
+            let x = bot.x;
+            let y = bot.y;
+            this.copyTo(bot);
+            bot.code[Math.round(Math.random()*bot.code.length)]+=Math.round(Math.random()*10)-5;
+            bot.energy/=2;
+            this.energy/=3;
+            bot.x = x;
+            bot.y = y;
+        } else {
+            this.energy/=2;
+            this.health-=bot.health/1.45;
+        }
     }
 }
 
@@ -104,14 +170,19 @@ function main() {
                 } else {
                     ctx.fillStyle = "rgb(3 3 3)";
                 }
-                ctx.fillRect(i*12, o*12, 11, 11);
+                let s = 820/width
+                ctx.fillRect(i*s, o*s, s, s);
                 
             }
         }
-        if(deadWorld)
+        if(deadWorld) {
+            //for (let i = 0; i < width; i++)
+                //for (let o = 0; o < height; o++)
+                    //console.log(matrix[i][o]);
             regenerateWorld();
+        }
 
-    }, 200);
+    }, 1000/40);
 }
 
 
@@ -128,8 +199,8 @@ function regenerateWorld() {
 
 function randomGen() {
     let t = [];
-    for(let i = 0; i < 10; i++) {
-        t.push(Math.round(Math.random()*40));
+    for(let i = 0; i < 64; i++) {
+        t.push(Math.round(Math.random()*16));
     }
     return t;
 }
