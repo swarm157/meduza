09e06f7ccd1816a6cfd6a2e450adeff57a4d62a9
Yra Kurbatov <108115642+swarm157@users.noreply.github.com>
2025-02-05 20:22:42 +0400

    123123

--- yra/script.js
+++ yra/script.js
@@ -6,8 +6,8 @@ let matrix = [];
 
 
 class Bot {
-    Bot(x, y, energy, code) {
-        this.health = 100;
+    constructor(x, y, energy, code) {
+        this.health = energy;
         this.x = x;
         this.y = y;
         this.energy = energy;
@@ -26,14 +26,19 @@ function main() {
 
     for(let i = 0; i < 25; i++) {
         matrix.push([]);
+        for(let o = 0; o < 25; o++) {
+            matrix[i][o] = new Bot(i, o, Math.round(Math.random()*200), null);
+            console.log(matrix[i][o]);
+        }
     }
     ctx.fillStyle = "rgb(200 0 0)";
     ctx.fillRect(12, 12, 11, 11);
+    console.log("initialized successful");
     setInterval(function() {
         for (let i = 0; i < 25; i++) {
             for (let o = 0; o < 25; o++) {
                 //console.log("i = "+i+" o = "+o);
-                ctx.fillStyle = "rgb(200 0 0)";
+                ctx.fillStyle = "rgb("+matrix[i][o].energy+" 0 0)";
                 ctx.fillRect(i*12, o*12, 11, 11);
             }
         }
