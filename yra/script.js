8a4351940c41c6114d5b5a0b997a75819d2b7a16
Yra Kurbatov <108115642+swarm157@users.noreply.github.com>
2025-02-05 20:36:49 +0400

    2

--- yra/script.js
+++ yra/script.js
@@ -7,15 +7,34 @@ let matrix = [];
 
 class Bot {
     constructor(x, y, energy, code) {
-        this.health = energy;
+        this.health = 100;
         this.x = x;
         this.y = y;
         this.energy = energy;
         this.code = code;
         this.state = 0;
+        this.alive = true;
     }
     next() {
-
+        if(this.state>this.code.length)
+            this.state = 0;
+        switch(this.state) {
+            case 0: break;
+            case 1: break;
+            case 2: break;
+            case 3: break;
+        }
+        if(this.energy<=0||this.health<=0) {
+            this.energy = 0;
+            this.alive = false;
+        } else if(this.energy>100) {
+            let d = 100-this.energy;
+            this.energy  = 100;
+            this.health -= d;
+        }
+        if(this.alive)
+            this.energy--;
+        this.state++;
     }
 }
 
@@ -27,8 +46,8 @@ function main() {
     for(let i = 0; i < 25; i++) {
         matrix.push([]);
         for(let o = 0; o < 25; o++) {
-            matrix[i][o] = new Bot(i, o, Math.round(Math.random()*200), null);
-            console.log(matrix[i][o]);
+            matrix[i][o] = new Bot(i, o, Math.round(Math.random()*200), randomGen());
+            //console.log(matrix[i][o]);
         }
     }
     ctx.fillStyle = "rgb(200 0 0)";
@@ -38,10 +57,25 @@ function main() {
         for (let i = 0; i < 25; i++) {
             for (let o = 0; o < 25; o++) {
                 //console.log("i = "+i+" o = "+o);
-                ctx.fillStyle = "rgb("+matrix[i][o].energy+" 0 0)";
+                if(matrix[i][o].alive) {
+                    matrix[i][o].next();
+                    ctx.fillStyle = "rgb("+matrix[i][o].energy+" "+matrix[i][o].health+" "+matrix[i][o].state+")";
+                } else {
+                    ctx.fillStyle = "rgb(3 3 3)";
+                }
                 ctx.fillRect(i*12, o*12, 11, 11);
+                
             }
         }
 
-    }, 1000);
+    }, 200);
+}
+
+
+function randomGen() {
+    let t = [];
+    for(let i = 0; i < 10; i++) {
+        t.push(Math.round(Math.random()*4));
+    }
+    return t;
 }
