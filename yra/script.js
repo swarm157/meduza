1037404550d68a6de2305f55a2bd51d71568ebd7
Yra Kurbatov <108115642+swarm157@users.noreply.github.com>
2025-02-05 19:11:26 +0400

    Update script.js

--- yra/script.js
+++ yra/script.js
@@ -5,7 +5,10 @@ window.onload = main;
 function main() {
     let cvs = document.getElementById("canvas");
     let ctx = cvs.getContext("2d");
-    setInterval(200, function() {
-        ctx.drawRect(0,0,200,200);
-    })
+    console.log(123123);
+    setInterval(function() {
+        console.log(13123);
+        ctx.fillStyle = "rgb(200 0 0)";
+        ctx.fillRect(10, 10, 50, 50);
+    }, 200);
 }
